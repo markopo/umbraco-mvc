4ea83d55f8e550c2ec64b5482e628c1df5fb54be
Marko <marko_poikkimaki@hotmail.com>
2016-05-15 10:09:05 +0200

    Umbraco SurfaceControllers

---
+++ UmbracoTest.Plugins/Controllers/MyPluginSurfaceController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc; 
+using Umbraco.Web.Mvc; 
+
+namespace UmbracoTest.Plugins.Controllers
+{
+    [PluginController("UmbracoTestPlugins")]
+    public class MyPluginSurfaceController : SurfaceController
+    {
+
+        public ActionResult Index()
+        {
+            return PartialView(); 
+        }
+
+    }
+}
---
+++ UmbracoTest/Controllers/ContactUsSurfaceController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc; 
+using Umbraco.Web.Mvc;
+using UmbracoTest.Models;
+using System.Xml.Linq; 
+
+namespace UmbracoTest.Controllers
+{
+    public class ContactUsSurfaceController : SurfaceController
+    {
+
+        public ActionResult Index(ContactUsModel model)
+        {
+            if(!string.IsNullOrEmpty(model.Name) && !string.IsNullOrEmpty(model.Email) && !string.IsNullOrEmpty(model.Message) )
+            {
+                var path = Server.MapPath("~/App_Data/ContactUs.xml");
+                var xdoc = XDocument.Load(path);
+                xdoc.Element("ContactUs").Add(new XElement("Contact",
+                    new XElement("Name", model.Name),
+                    new XElement("Email", model.Email),
+                    new XElement("Message", new XCData(model.Message))));
+                xdoc.Save(path); 
+
+
+            }
+
+            return RedirectToCurrentUmbracoPage(); 
+        }
+
+    }
+}
